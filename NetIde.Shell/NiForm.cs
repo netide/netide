3521ec10ccdd22fd8dfbeaa4f309fd046e045b91
Pieter van Ginkel <pvginkel@gmail.com>
2014-11-28 11:56:54 +0100

    Added HelpTopic property to the NiForm base form.

--- NetIde.Shell/NiForm.cs
+++ NetIde.Shell/NiForm.cs
@@ -1,20 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using NetIde.Shell.Interop;
-using NetIde.Util.Forms;
+using Form = NetIde.Util.Forms.Form;
 
 namespace NetIde.Shell
 {
     public class NiForm : Form
     {
+        private static readonly Regex HelpTopicRe = new Regex("^([^/:]+):(.+)$", RegexOptions.Compiled);
+
         private int _cookie;
+        private string _helpTopic = String.Empty;
+
         public NiCommandMapper CommandMapper { get; private set; }
+
+        [Category("Behavior")]
+        [DefaultValue("")]
+        public string HelpTopic
+        {
+            get { return _helpTopic; }
+            set
+            {
+                if (value == null)
+                    value = String.Empty;
+
+                if (!HelpTopicRe.IsMatch(value))
+                    throw new ArgumentException("Help topic must be formatted as \"root:path\"");
+
+                if (_helpTopic != value)
+                {
+                    _helpTopic = value;
+                    HelpButton = value.Length > 0;
+                }
+            }
+        }
 
         public NiForm()
         {
             CommandMapper = new NiCommandMapper();
+
+            CommandMapper.Add(
+                NiResources.Help_ViewHelp,
+                p => ShowHelp()
+            );
+        }
+
+        protected override void OnHelpButtonClicked(CancelEventArgs e)
+        {
+            base.OnHelpButtonClicked(e);
+
+            e.Cancel = true;
+
+            ShowHelp();
+        }
+
+        private void ShowHelp()
+        {
+            if (_helpTopic.Length > 0)
+            {
+                var match = HelpTopicRe.Match(_helpTopic);
+
+                ErrorUtil.ThrowOnFailure(((INiHelp)GetService(typeof(INiHelp))).Navigate(
+                    match.Groups[1].Value,
+                    match.Groups[2].Value
+                ));
+            }
         }
 
         protected override void OnShown(EventArgs e)
